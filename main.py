bd75f2a3b0fbf30c258d1098a27eee886a29b9df
Morita Taro <38848223+taro0079@users.noreply.github.com>
2024-07-07 13:49:56 +0900

    chore: Refactor main.py for barcode scanning functionality and API integration

--- main.py
+++ main.py
@@ -1,43 +1,13 @@
-import array
 import base64
-from cmath import rect
+from typing import Any, List, Dict
 import cv2 as cv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import numpy as np
 from pyzbar.pyzbar import decode
+import requests
 
-# cap = cv.VideoCapture(0)
-# cap.set(3, 640)
-# cap.set(4, 480)
-
-# while True:
-#     ret, frame = cap.read()
-#     for barcode in decode(frame):
-#         myData = barcode.data.decode("utf-8")
-#         print(myData)
-#         pts = np.array([barcode.polygon], np.int32)
-
-#         cv.polylines(frame, [pts], True, (0, 255, 0), 5)
-
-#         pts2 = barcode.rect
-#         cv.putText(
-#             frame,
-#             myData,
-#             (pts2[0], pts2[1]),
-#             cv.FONT_HERSHEY_SIMPLEX,
-#             0.9,
-#             (255, 0, 0),
-#             2,
-#         )
-
-#     cv.imshow("test", frame)
-
-#     if cv.waitKey(1) & 0xFF == ord("q"):
-#         break
-
-# cap.release()
-# cv.destroyAllWindows()
+# cap = cv.VideoCapture(0
 
 
 app = Flask(__name__)
@@ -46,17 +16,23 @@ CORS(app)
 
 @app.route("/decode", methods=["POST"])
 def decode_barcode():
-    data = request.json["image"]
+    if not request.json or "image" not in request.json:
+        return jsonify({"error": "No image data found"})
+    data: str = request.json.get("image")
+
     image_data = base64.b64decode(data)
     np_arr = np.frombuffer(image_data, np.uint8)
     img = cv.imdecode(np_arr, cv.IMREAD_COLOR)
     # cv.imwrite("test.jpg", img)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    barcodes = decode(gray)
-    result = []
+    # barcodes = decode(gray)
+    barcodes: List[Any] = decode(img)
+
+    result: List[Dict[str, Any]] = []
     for barcode in barcodes:
         barcode_data = barcode.data.decode("utf-8")
+        call_product_api(barcode_data)
         rect = barcode.rect
         result.append(
             {
@@ -71,6 +47,14 @@ def decode_barcode():
         )
     print(result)
     return jsonify(result)
+
+
+def call_product_api(barcode_data: str):
+    url = "http://localhost:3000/products/code/{}".format(barcode_data)
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+    response = requests.get(url, headers=headers)
+    print(response.json())
 
 
 if __name__ == "__main__":
